a38a117c04bfc226de733494e33d904315132096
Arya Anantula <43790515+Arya333@users.noreply.github.com>
2019-12-16 20:18:18 -0600

    array

--- Connect4/src/main/java/Connect4/MainWindow.java
+++ Connect4/src/main/java/Connect4/MainWindow.java
@@ -5,17 +5,40 @@
  */
 package Connect4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author anant
  */
 public class MainWindow {
+    
+    int rows = 6;
+    int columns = 7;
+    int[][] array = new int[rows][columns];
+    
+    public MainWindow(){
+        
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        boolean userChoice = true;
+        System.out.println("Do you want to keep playing (Y/N)?"); 
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan.nextLine());
+        
     }
     
+    public void printArray(){
+        
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < columns; j++){
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
